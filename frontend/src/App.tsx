2626232e3708ccbc35afb10387ac0774e5ab4c45
Yash <yashkumar982000@gmail.com>
2024-03-09 20:27:00 +0530

    Fixed default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ export const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignIn />} />
+          <Route path="/" element={<Blogs />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/blogs" element={<Blogs />} />
